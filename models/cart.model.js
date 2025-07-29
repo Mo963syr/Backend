1d76e7a27b3ff8356289ed58daf887ba8f469fac
Mo963syr <moafaqaqeed01@gmail.com>
2025-07-29 14:02:05 +0300

    dd

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,43 +1,52 @@
 const mongoose = require('mongoose');
 
-const cartSchema = new mongoose.Schema({
-  partId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Part',
-    required: [true, 'يجب ربط القطعة'],
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'يجب ربط المستخدم'],
-  },
-  quantity: {
-    type: Number,
-    default: 1,
-    min: 1,
-  },
-  status: {
-    type: String,
-    enum: ['قيد المعالجة', 'مؤكد', 'ملغي'],
-    default: 'قيد المعالجة',
-  },
-  paymentMethod: {
-    type: String,
-    enum: ['عند الاستلام', 'بطاقة إلكترونية'],
-    default: 'عند الاستلام',
-  },
-  location: {
-    type: {
-      type: String,
-      enum: ['Point'],
-      default: 'Point'
+const cartSchema = new mongoose.Schema(
+  {
+    partId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Part',
+      required: [true, 'يجب ربط القطعة'],
     },
-    coordinates: {
-      type: [Number], // [lng, lat]
-      default: undefined
-    }
-  }
-}, { timestamps: true });
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'يجب ربط المستخدم'],
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+      min: 1,
+    },
+    status: {
+      type: String,
+      enum: ['قيد المعالجة', 'مؤكد', 'ملغي'],
+      default: 'قيد المعالجة',
+    },
+    paymentMethod: {
+      type: String,
+      enum: ['عند الاستلام', 'بطاقة إلكترونية'],
+      default: 'عند الاستلام',
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        default: 'Point',
+      },
+      coordinates: {
+        type: [Number],
+        validate: {
+          validator: function (val) {
+            return Array.isArray(val) && val.length === 2;
+          },
+          message: 'يجب تحديد إحداثيات صحيحة [lng, lat]',
+        },
+        required: false,
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 cartSchema.index({ location: '2dsphere' });
 
