5c1772bd09c8a4b638aa781f2602d80c0fea81f5
Mo963syr <moafaqaqeed01@gmail.com>
2025-08-15 21:43:46 +0300

    login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -46,3 +46,45 @@ exports.register=async(req,res)=>{
 
 
 };
+
+exports.login=async(req,res)=>{
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const response = {
+      message: 'Sign in successful',
+   
+      userId: user._id,
+
+      role: user.role,
+    };
+
+    if (user.role === 'doctor') {
+      response.status = 'doctor dashboard';
+    } else if (user.role === 'user') {
+      response.status = 'user dashboard';
+    } else if (user.role === 'coordinator') {
+      response.status = 'coordinator dashboard';
+    } else if (user.role === 'employee') {
+      response.status = 'employee dashboard';
+    }
+
+    return res.status(200).json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+
+
+
+};
--- routes/user.Routes.js
+++ routes/user.Routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {addUser,viewsellerprands } = require('../controllers/user.Controller');
-const {register} = require('../controllers/auth.controller');
+const {register,login} = require('../controllers/auth.controller');
 
 router.post('/add',addUser);
 router.post('/register',register);
+router.post('/login',login);
 router.get('/viewsellerprands/:userId', viewsellerprands);
 
 module.exports = router;
